e65fee0df501abc0a4c6e0596c35702847851a7c
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2025-03-01 20:59:25 +0000

    allow webapp to pull image

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_container_registry_scope_map" "acr_writer" {
   container_registry_name = azurerm_container_registry.acr.name
   resource_group_name     = azurerm_resource_group.rg.name
   actions = [
-    "repositories/repo1/content/write"
+    "repositories/fastapi-cosmos-app/content/write"
   ]
 }
 
@@ -74,6 +74,12 @@ resource "azurerm_container_registry_token_password" "acr_writer_token_password"
   container_registry_token_id = azurerm_container_registry_token.acr_writer_token.id
   password1 {
   }
+}
+
+resource "azurerm_role_assignment" "webapp_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
 }
 
 resource "azurerm_linux_web_app" "webapp" {
