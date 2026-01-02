2b6b3e9a16b77e2bb27ef207b20738b452dc7b34
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2026-01-02 17:53:55 +0000

    Refactor role assignments in Terraform files for Azure Cosmos DB and remove redundant code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,15 +10,3 @@ resource "random_string" "random" {
   lower   = true
   numeric = false
 }
-
-# resource "azurerm_role_assignment" "webapp_acr_pull" {
-#   scope                = azurerm_container_registry.acr.id
-#   role_definition_name = "AcrPull"
-#   principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
-# }
-
-# resource "azurerm_role_assignment" "webapp_slot_acr_pull" {
-#   scope                = azurerm_container_registry.acr.id
-#   role_definition_name = "AcrPull"
-#   principal_id         = azurerm_linux_web_app_slot.webapp_slot.identity[0].principal_id
-# }
--- infrastructure/webapp.tf
+++ infrastructure/webapp.tf
@@ -54,22 +54,6 @@ resource "azurerm_linux_web_app_slot" "webapp_slot" {
   }
 }
 
-resource "azurerm_cosmosdb_sql_role_assignment" "ra_cosmos_webapp" {
-  resource_group_name = azurerm_resource_group.rg.name
-  scope               = azurerm_cosmosdb_account.cosmos.id
-  account_name        = azurerm_cosmosdb_account.cosmos.name
-  role_definition_id  = azurerm_cosmosdb_sql_role_definition.role_definition.id
-  principal_id        = azurerm_linux_web_app.webapp.identity[0].principal_id
-}
-
-resource "azurerm_cosmosdb_sql_role_assignment" "ra_cosmos_webapp_slot" {
-  resource_group_name = azurerm_resource_group.rg.name
-  scope               = azurerm_cosmosdb_account.cosmos.id
-  account_name        = azurerm_cosmosdb_account.cosmos.name
-  role_definition_id  = azurerm_cosmosdb_sql_role_definition.role_definition.id
-  principal_id        = azurerm_linux_web_app_slot.webapp_slot.identity[0].principal_id
-}
-
 resource "azurerm_app_service_virtual_network_swift_connection" "vnet_webapp" {
   app_service_id = azurerm_linux_web_app.webapp.id
   subnet_id      = azurerm_subnet.webapp_subnet.id
